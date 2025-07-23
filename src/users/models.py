ded238616f42289a7910cb972fa102190182b87d
simondrugan16 <119412400+simondrugan16@users.noreply.github.com>
2025-07-23 11:59:50 +0100

    created user profile model and applied migration to database

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(null=True)
+    bio = models.CharField(max_length=140, blank=True)
+    phone_number = models.CharField(max_length=12, blank=True)
